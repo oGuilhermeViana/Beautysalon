0647af703efe10791dda8472671ac95c2694f3b6
Guilherme Viana <44452847+oGuilhermeViana@users.noreply.github.com>
2022-05-23 16:17:49 -0300

    fim da aula Aula 02 - Maximum Speed

--- js/main.js
+++ js/main.js
@@ -16,3 +16,16 @@ for (const link of links) {
         nav.classList.remove('show')
     })
 }
+
+/* box-shadow quando ter scroll*/ 
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function(){
+    if (window.scrollY >= navHeight){
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
